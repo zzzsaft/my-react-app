21fef2cda6019eea64ef28690fa6b39e97296a7e
zzzsaft <55004427+zzzsaft@users.noreply.github.com>
2025-06-13 20:59:23 +0800

    Move default terms control into form

--- src/components/quote/QuoteTermsTab.tsx
+++ src/components/quote/QuoteTermsTab.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { Button } from "antd";
 import TermsTable from "./TermsTable";
 import { Clause } from "../../types/types";
 
 interface QuoteTermsTabProps {
   value: Clause[];
   onChange: (terms: Clause[]) => void;
+  onSetDefault: () => void;
 }
 
-const QuoteTermsTab: React.FC<QuoteTermsTabProps> = ({ value, onChange }) => {
-  return <TermsTable value={value} onChange={onChange} />;
+const QuoteTermsTab: React.FC<QuoteTermsTabProps> = ({
+  value,
+  onChange,
+  onSetDefault,
+}) => {
+  return (
+    <>
+      <Button type="dashed" onClick={onSetDefault} style={{ marginBottom: 16 }}>
+        设置默认条款
+      </Button>
+      <TermsTable value={value} onChange={onChange} />
+    </>
+  );
 };
 
 export default QuoteTermsTab;
